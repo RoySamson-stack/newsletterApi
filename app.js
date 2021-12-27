66d57b4986b721d5e0c6c1b983b7f70b153c5f11
RoySamson-stack <rsam9009@gmail.com>
2021-12-27 22:35:08 +0300

    adding the getting of data from the form

--- app.js
+++ app.js
@@ -4,7 +4,22 @@ const request = require('request');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.post("/", function (req, res) {
+ 
+  var firstName = req.body.fname
+  var lastName = req.body.lname
+  var email = req.body.email
+  console.log(lastName, firstName, email)
+ 
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
 app.listen(3000, () => {
   console.log('listening on port 3000');
 })
-
